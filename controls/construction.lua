dd7b4d2fdce292927ce446de03511edb84998dac
Stuart P. Bentley <Stuart@testtrack4.com>
2010-04-26 08:41:26 -0700

    Finished new design: - controls are now colored according to the color of their line (added settings/colors.lua to store) - removed the Sum label and replaced the VOLTORB label with the game's voltorb image indented into the control - moved controls to center and widened them - replaced arbitrary row control space with the width for controls

--- controls/construction.lua
+++ controls/construction.lua
@@ -11,8 +11,12 @@ require "iuplua"
 
 --Required for both sizing and positioning
 local sizes = require "settings.sizes"
+--Required for background colors
+local colors = require "settings.colors"
 --Required for initializing the default text values.
 local formatters = require "controls.formatting"
+--Required for Voltorb label.
+local voltorb = require "images.voltorb"
 
 -------------------------------------------------------------------------------
 -- "Constant" value definitions
@@ -21,6 +25,7 @@ local formatters = require "controls.formatting"
 --Gets used for stuff like where to position the column controls
 --(below the bottom of the canvas)
 local canvassize=(sizes.card+sizes.cardgap)*lines
+local voltwidth=voltorb.width
 
 -------------------------------------------------------------------------------
 -- Module functionality
@@ -30,18 +35,14 @@ local canvassize=(sizes.card+sizes.cardgap)*lines
 local function construct_line_controls(
   axis, --The axis to create on ("rows" or "columns").
   position, --The row/column to create controls for.
-  textboxes, --The table containing the textboxes for rows and columns.
-  labels, --The table containing the labels for rows and columns.
+  lineboxes, --The table containing this line's table
+             --for storing the sum and Voltorb controls.
+  axislabels, --The table containing the labels of all this axis's lines.
   defaults --The default numbers for the sum and voltorb textboxes.
   )
 
-  --Localize this line's label and textbox tables.
-  local linelabels=labels[axis][position]
-  local lineboxes=textboxes[axis][position]
-
   --Positions for controls:
   local left  --left side of the labels
-  local textleft  --left side of the textboxes
   local tops={} --tops of the Voltorb and Sum controls
 
   --If controls are for a row
@@ -52,12 +53,6 @@ local function construct_line_controls(
       + canvassize -- plus the width of the canvas
       + sizes.controls.gap -- plus the gap between the labels and the canvas
 
-    textleft = -- The left of the textboxes is
-      left -- the left of the labels
-      + sizes.rspace -- plus the width of the entire right
-      - sizes.controls.textbox.width -- minus the width of the textboxes
-      - sizes.margin -- minus the right margin
-
     tops.sum = -- The top of the Sum controls is
       sizes.margin -- the top margin of the window
       + (sizes.card + sizes.cardgap) -- plus the cumulative
@@ -67,12 +62,12 @@ local function construct_line_controls(
       - sizes.card/2 -- and half of its card height to center it
       - sizes.controls.gap/2 -- minus half of the gap between controls
                              -- to center the gap between sum and voltorb
-      - sizes.controls.textbox.height -- minus the height of the Sum box
+      - sizes.controls.height -- minus the height of the Sum box
                                       -- to place it above the middle
 
     tops.voltorb = -- The top of the Voltorb controls is
       tops.sum -- the top of the Sum controls
-      + sizes.controls.textbox.height -- plus the height of the Sum textbox
+      + sizes.controls.height -- plus the height of the Sum textbox
       + sizes.controls.gap -- plus the gap between the controls
 
   --Otherwise if controls are for a column
@@ -83,11 +78,8 @@ local function construct_line_controls(
       + (sizes.card + sizes.cardgap) -- plus the cumulative
                                      -- card widths and gaps
         * (position-1) -- of every row before this one
-
-    textleft = -- The left of the textboxes is
-      left -- the left of the labels
-      + sizes.card -- plus the width of the card
-      - sizes.controls.textbox.width -- minus the width of the textboxes
+      + sizes.card/2
+      - sizes.controls.width/2
 
     tops.sum = -- The top of the Sum controls is
       sizes.margin -- the top margin of the window
@@ -96,7 +88,7 @@ local function construct_line_controls(
 
     tops.voltorb = -- The top of the Voltorb controls is
       tops.sum -- the top of the Sum controls
-      + sizes.controls.textbox.height -- plus the height of the Sum textbox
+      + sizes.controls.height -- plus the height of the Sum textbox
       + sizes.controls.gap -- plus the gap between the textboxes
 
   -- if it's not rows or columns then there's a mistake somewhere
@@ -104,16 +96,16 @@ local function construct_line_controls(
   end
 
   --Create this line's labels
-  linelabels.sum= iup.label{title="Sum:", cx=left,cy=tops.sum}
-  linelabels.voltorb= iup.label{title="VOLTORB:", cx=left,cy=tops.voltorb}
 
-  --save the string for the size for textboxes
-  local textboxsize = sizes.wxh(sizes.controls.textbox.width,
-    sizes.controls.textbox.height)
+  axislabels[position]= iup.label{image=voltorb, cx=left,
+  cy=tops.voltorb+(sizes.controls.height-voltorb.height)/2}
+
+  local bg=string.format("%i %i %i", unpack(colors.lines[position]))
 
   --Function for creating both textboxes.
-  local function maketextbox (box, max)
+  local function maketextbox (box, max, indent)
     lineboxes[box] = iup.text{
+      bgcolor=bg,
       mask = iup.MASK_UINT, -- allow only digits
       alignment = "ARIGHT", --just like in the games
       spin = "YES", -- give us those incrementing arrows on the side and all
@@ -123,15 +115,16 @@ local function construct_line_controls(
       spinvalue = defaults[box], -- start at the default for this box
       value = -- start the text with the formatted default
         formatters[box](defaults[box]),
-      rastersize = textboxsize, -- use the size specified for textboxes
-      cx = textleft, --position the left at the left for textboxes
+      rastersize = sizes.wxh(sizes.controls.width-indent,
+        sizes.controls.height),
+      cx = left+indent, --position the left at the left for textboxes
       cy = tops[box] --position the top at the top for this textbox
     }
   end
 
   --Make the textboxes
-  maketextbox('sum', 3*lines)
-  maketextbox('voltorb', lines)
+  maketextbox('sum', 3*lines, 0)
+  maketextbox('voltorb', lines,voltwidth+sizes.controls.gap)
 end
 
 --Module Function
@@ -144,11 +137,12 @@ return function (textboxes,labels,defaults)
   for _, axis in pairs{"rows","columns"} do
     --For each line
     for line=1, lines do
-      --Create tables for this line in both control tables
-      textboxes[axis][line]={}
-      labels[axis][line]={}
+      --Create the table to store this line's textboxes in
+      local lineboxes={}
       --Construct this line's labels and textboxes.
-      construct_line_controls(axis,line,textboxes,labels,defaults)
+      construct_line_controls(axis,line,lineboxes,labels[axis],defaults)
+      --Store this line's controls in the greater table
+      textboxes[axis][line]=lineboxes
     end
   end
 end
--- settings/sizes.lua
+++ settings/sizes.lua
@@ -7,16 +7,13 @@ local sizes = {
   bars=16,
   --Margin between window edge and all controls
   margin=3,
-  --Space for controls to right of canvas
-  rspace=100,
   controls={
     --Gap between controls
     gap=2,
-    --Sizes of textboxes
-    textbox={
-      width=40,
-      height=20
-    },
+    --Width of textbox groups
+    width=60,
+    --Heights of textboxes
+    height=20
   },
 }
 
